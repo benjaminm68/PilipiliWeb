ce16614dc5a3461290e3ae5cb14243e26cc5425a
Benjamin68100 <61730304+Benjamin68100@users.noreply.github.com>
2021-08-15 22:12:14 +0200

    Création d'une commande pour activer un produit

---
+++ src/Command/ProductActivationCommand.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+class ProductActivationCommand extends Command
+{
+    protected static $defaultName = 'app:product-activation';
+    protected static $defaultDescription = 'Gérer l\'activation des produits';
+
+    private $em;
+    private $productRepository;
+
+    public function __construct(EntityManagerInterface $em, ProductRepository $productRepository)
+    {
+        $this->em = $em;
+        $this->productRepository = $productRepository;
+
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Activer un produit');
+        $this->addArgument('id', InputArgument::OPTIONAL, 'ID du produit à activer');
+        $this->addArgument('activated', InputArgument::OPTIONAL, 'Oui ou nan');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        
+        $output->writeln([
+            '==================',
+            'Activer un produit',
+            '==================',
+            '',
+        ]);
+        
+        $helper = $this->getHelper('question');        
+
+        $productIdQuestion = new Question('Indiquez l\'id du produit à modifier: ');
+        $resultId = $helper->ask($input, $output, $productIdQuestion);
+
+        if($this->productRepository->find($resultId)){
+            //On indique que le produit demandé à bien été trouvé
+            $output->writeln('Produit trouvé');
+
+            //On propose de l'activer ou non
+            $productActiveQuestion = new ChoiceQuestion(
+                'Voulez vous activer le produit ? (Non par defaut) ',
+                ['Non', 'Oui'],
+                0
+            );
+
+            //On sctock le résultat dans la variable
+            $resultActivation = $helper->ask($input, $output, $productActiveQuestion);
+            
+            //Selon le choix de l'utilisateur la valeur du résultat est changé
+            if($resultActivation === "Oui"){
+                $resultActivation = 1;
+            }else{
+                $resultActivation = 0;
+            }
+
+            // On recherche le produit avec l'id indiqué
+            $productFind = $this->productRepository->find($resultId);
+
+            // On change la valeur de enabled
+            $productFind->setEnabled($resultActivation);
+            $this->em->persist($productFind);
+            $this->em->flush();
+            
+            if($resultActivation === 1){
+                $output->writeln('Le produit a été activé');
+            }else{
+                $output->writeln('Le produit a été désactivé');
+            }
+
+        }else{
+            $output->writeln('Produit non trouvé');
+            die;
+        }
+        
+
+
+        return Command::SUCCESS;
+    }
+}
